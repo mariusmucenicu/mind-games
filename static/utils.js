d472b0f57fc3eb7cffc1299a91a4126bd51face5
Marius Mucenicu <marius_mucenicu@yahoo.com>
2018-05-09 08:39:05 +0300

    Fix I'm feeling lucky btn and refactor js style

--- static/utils.js
+++ static/utils.js
@@ -1,9 +1,9 @@
 function randomint() {
-  var raw_string = document.getElementById('user-gamble').elements['raw_data'].value;
-  raw_string = JSON.parse(raw_string.replace(/'/g, '"'));
-  left_bound = raw_string.left_bound;
-  right_bound = raw_string.right_bound;
-  random_number = Math.floor(Math.random() * (right_bound-left_bound+1) + left_bound);
+  var rawString = document.getElementById('user-gamble').elements['raw_data'].value;
+  rawString = JSON.parse(rawString.replace(/'/g, '"'));
+  lowerBound = rawString.start;
+  upperBound = rawString.stop;
+  random_number = Math.floor(Math.random() * (upperBound-lowerBound+1) + lowerBound);
   document.getElementById('user-gamble').elements['answer'].value = random_number
 }
 
