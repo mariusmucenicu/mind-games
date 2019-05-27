f964ea5ef8754399ad5c9ba5babff3f4f8a9ac59
Marius Mucenicu <marius_mucenicu@yahoo.com>
2019-05-27 07:45:01 +0300

    backend: Add check constraints and remove length args
    Resolves #178

--- knowlift/models.py
+++ knowlift/models.py
@@ -31,7 +31,7 @@ user = sqlalchemy.Table(
     'user',
     metadata,
     sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column('username', sqlalchemy.String(length=255), unique=True, nullable=False),
+    sqlalchemy.Column('username', sqlalchemy.String, unique=True, nullable=False),
     sqlalchemy.Column('email', sqlalchemy.String, unique=True, nullable=False),
     sqlalchemy.Column('password', sqlalchemy.String, nullable=False),
     sqlalchemy.Column('country_id', sqlalchemy.ForeignKey('country.id'), nullable=False),
@@ -40,15 +40,36 @@ user = sqlalchemy.Table(
     sqlalchemy.Column('last_login', sqlalchemy.DateTime),
     sqlalchemy.Column('is_active', sqlalchemy.Boolean, default=False),
     sqlalchemy.Column('is_staff', sqlalchemy.Boolean, default=False),
-    sqlalchemy.Column('first_name', sqlalchemy.String(length=255)),
-    sqlalchemy.Column('last_name', sqlalchemy.String(length=255)),
+    sqlalchemy.Column('first_name', sqlalchemy.String),
+    sqlalchemy.Column('last_name', sqlalchemy.String),
 )
 
 country = sqlalchemy.Table(
     'country',
     metadata,
-    sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column('english_short_name', sqlalchemy.String, unique=True, nullable=False),
-    sqlalchemy.Column('alpha2_code', sqlalchemy.String(length=2), unique=True, nullable=False),
-    sqlalchemy.Column('alpha3_code', sqlalchemy.String(length=3), unique=True, nullable=False),
+    sqlalchemy.Column(
+        name='id',
+        type_=sqlalchemy.Integer,
+        primary_key=True,
+    ),
+    sqlalchemy.Column(
+        name='english_short_name',
+        type_=sqlalchemy.String,
+        unique=True,
+        nullable=False
+    ),
+    sqlalchemy.Column(
+        sqlalchemy.CheckConstraint('length(alpha2_code) <= 2', name='max_length'),
+        name='alpha2_code',
+        type_=sqlalchemy.String,
+        unique=True,
+        nullable=False,
+    ),
+    sqlalchemy.Column(
+        sqlalchemy.CheckConstraint('length(alpha3_code) <= 3', name='max_length'),
+        name='alpha3_code',
+        type_=sqlalchemy.String,
+        unique=True,
+        nullable=False,
+    ),
 )
